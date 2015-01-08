e38a67f40e4f0c6b4a7bb34d5ef66ea6128c280d
fmaruri <fernanda.maruri@vanderbilt.edu>
2015-01-07 22:25:07 -0600

    <commit mesage>

--- Plot2.R
+++ Plot2.R
@@ -19,27 +19,10 @@ f$Sub_metering_1 <- as.numeric(as.character(f$Sub_metering_1))
 f$Sub_metering_2 <- as.numeric(as.character(f$Sub_metering_2))
 f$Sub_metering_3 <- as.numeric(as.character(f$Sub_metering_3))
 
-##CREATE PLOT 4
-plot4 <- function() {
-        par(mfrow=c(2,2))
-        
-        ##PLOT A
-        plot(f$timestamp,f$Global_active_power, type="l", xlab="", ylab="Global Active Power")
-       
-	##PLOT B
-        plot(f$timestamp,f$Voltage, type="l", xlab="datetime", ylab="Voltage")
-        
-        ##PLOT C
-        plot(f$timestamp,f$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-        lines(f$timestamp,f$Sub_metering_2,col="red")
-        lines(f$timestamp,f$Sub_metering_3,col="blue")
-        legend("topright", col=c("black","red","blue"), c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),lty=c(1,1), bty="n", cex=.5) 
-        
-        ##PLOT D
-        plot(f$timestamp,f$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
-        
-        ##OUTPUT
-        dev.copy(png, file="plot4.png", width=480, height=480)
-        dev.off()
-     }
-plot4()
+##CREATE PLOT 2
+plot2 <- function() {
+        plot(f$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+        dev.copy(png, file="plot2.png", width=480, height=480)
+        dev.off()}
+
+plot2()
